59f338ae9b6ef73b61cf519b2dac3170eeda3ce9
Felipe Melo <narulee@hotmail.com>
2022-06-24 19:31:58 -0300

    displaying cities dynamically

--- frontend/src/components/GroupList/GroupItem.jsx
+++ frontend/src/components/GroupList/GroupItem.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { ConfirmationAlert } from "../ConfirmationAlert";
 import { ActionButton } from "./ActionButton";
+import { CityItem } from "./CityItem";
 
-export function GroupItem() {
+export function GroupItem({ name, cities }) {
   const [isDeleteAlertOpen, setIsDeleteAlertOpen] = useState(false)
 
   function handleOpenDeleteAlert() {
@@ -16,10 +17,12 @@ export function GroupItem() {
   return (
     <div className="bg-slate-100 p-6 flex items-center justify-between shadow-sm rounded-md flex-wrap">
       <div className="flex flex-col">
-        <span className="text-xl font-bold">Sudeste</span>
-        <span>
-          Minas Gerais, Rio de Janeiro, São Paulo, Espírito Santo
-        </span>
+        <span className="text-xl font-bold">{name}</span>
+        <div className="flex gap-1 mt-2">
+          {cities.map(city => (
+            <CityItem uf={city.uf} city={city.name} />
+          ))}
+        </div>
       </div>
       <div className="flex justify-end gap-2">
         <ActionButton action="edit" onClick={() => { }} />
--- frontend/src/components/GroupList/index.jsx
+++ frontend/src/components/GroupList/index.jsx
@@ -1,21 +1,29 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 import { GroupHeader } from "./GroupHeader";
 import { GroupItem } from "./GroupItem";
 
+import api from "../../services/api";
+
 export function GroupList() {
+  const [groups, setGroups] = useState([])
+
+  useEffect(() => {
+    api.get('group')
+      .then(response => setGroups(response.data))
+      .catch(err => console.log(err.message))
+  }, [])
+
+  console.log(groups)
+
   return (
     <>
       <GroupHeader />
       <div className="flex flex-col gap-2 overflow-y-auto h-[60vh]">
-        <GroupItem />
-        <GroupItem />
-        <GroupItem />
-        <GroupItem />
-        <GroupItem />
-        <GroupItem />
-        <GroupItem />
-        <GroupItem />
-        <GroupItem />
-        <GroupItem />
+        {groups.map(group => (
+          <GroupItem key={group.id} name={group.name} cities={group.cities} />
+        ))}
       </div>
     </>
   )
