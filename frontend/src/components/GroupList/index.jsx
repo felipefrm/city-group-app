dd1599cd5944a7e9ef22a89fc443075860541f40
Felipe Melo <narulee@hotmail.com>
2022-06-25 11:51:20 -0300

    moved AddGroup logic to GroupList

--- frontend/src/components/GroupList/index.jsx
+++ frontend/src/components/GroupList/index.jsx
@@ -1,19 +1,40 @@
+import { useState } from "react";
+
 import { GroupHeader } from "./GroupHeader";
 import { GroupItem } from "./GroupItem";
+import { AddGroupModal } from "../Modal/AddGroupModal";
+import { AddGroupButton } from "./AddGroupButton";
 
 import { useGroups } from "../../contexts/groups";
 
 export function GroupList() {
   const { groups } = useGroups()
 
+  const [isNewGroupModalOpen, setIsNewGroupModalOpen] = useState(false)
+
+  function handleOpenNewGroupModal() {
+    setIsNewGroupModalOpen(true)
+  }
+
+  function handleCloseNewGroupModal() {
+    setIsNewGroupModalOpen(false)
+  }
+
   return (
-    <>
+    <main className="flex flex-col gap-2 max-w-[1440px] m-auto px-4 -mt-32">
+      <AddGroupButton onClick={handleOpenNewGroupModal} />
+
       <GroupHeader />
       <div className="flex flex-col gap-2 overflow-y-auto h-[60vh]">
         {groups.map(group => (
           <GroupItem key={group.id} group={group} />
         ))}
       </div>
-    </>
+
+      <AddGroupModal
+        isOpen={isNewGroupModalOpen}
+        closeModal={handleCloseNewGroupModal}
+      />
+    </main>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,34 +3,15 @@ import { useState } from "react";
 import { Footer } from "../components/Footer";
 import { GroupList } from "../components/GroupList";
 import { Header } from "../components/Header";
-import { AddGroupButton } from "../components/GroupList/AddGroupButton";
-import { AddGroupModal } from "../components/Modal/AddGroupModal";
+
 import { GroupsProvider } from "../contexts/groups";
 
 export function Home() {
-  const [isNewGroupModalOpen, setIsNewGroupModalOpen] = useState(false)
-
-  function handleOpenNewGroupModal() {
-    setIsNewGroupModalOpen(true)
-  }
-
-  function handleCloseNewGroupModal() {
-    setIsNewGroupModalOpen(false)
-  }
-
   return (
     <GroupsProvider>
       <Header />
-      <main className="flex flex-col gap-2 max-w-[1440px] m-auto -mt-32">
-        <AddGroupButton onClick={handleOpenNewGroupModal} />
-        <GroupList />
-      </main>
+      <GroupList />
       <Footer />
-
-      <AddGroupModal
-        isOpen={isNewGroupModalOpen}
-        closeModal={handleCloseNewGroupModal}
-      />
     </GroupsProvider>
   )
 }
