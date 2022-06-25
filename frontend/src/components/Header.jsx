4ef6e727d76fe96d48b60385e97f0a974743d27d
Felipe Melo <narulee@hotmail.com>
2022-06-25 11:24:58 -0300

    logged user name

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,20 +1,23 @@
+import { useNavigate, Link } from 'react-router-dom';
+
 import { useAuth } from '../contexts/auth'
 
 import logoImg from '../assets/images/logo.png'
 import userImg from '../assets/images/default-user-image.png'
-import { useNavigate } from 'react-router-dom';
 
 export function Header() {
-  const { signOut } = useAuth()
+  const { signOut, user } = useAuth()
   let navigate = useNavigate();
 
   return (
     <header className="bg-slate-300 h-64">
       <div className="max-w-[1440px] mx-auto flex items-center justify-between">
-        <img src={logoImg} alt="Economapas" className="max-w-xs py-2" />
+        <Link to="/">
+          <img src={logoImg} alt="Economapas" className="max-w-xs py-2" />
+        </Link>
         <div className="flex mr-6 items-center">
           <div className="flex flex-col mr-2">
-            <span className="font-bold">João</span>
+            <span className="font-bold">{user.name}</span>
             <button
               type="button"
               onClick={() => signOut(() => navigate("/login"))}
