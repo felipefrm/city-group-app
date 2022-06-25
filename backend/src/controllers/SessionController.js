7a746c5b97caaab50832efb5ff65a55a09387b2c
Felipe Melo <narulee@hotmail.com>
2022-06-25 12:47:41 -0300

    close db connection, .env db connection

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -1,4 +1,4 @@
-require("dotenv-safe").config();
+require("dotenv").config();
 const jwt = require('jsonwebtoken');
 const runQuery = require("../database/runQuery");
 
--- backend/src/database/runQuery.js
+++ backend/src/database/runQuery.js
@@ -1,14 +1,17 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 async function runQuery(query, params = null) {
   const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'economapas'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_SCHEMA,
   });
 
   const [rows] = await connection.execute(query, params);
+
+  await connection.end()
 
   return rows
 }
