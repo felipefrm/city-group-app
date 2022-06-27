e7d41be151fbc5f482467d58abb08045d5e8dcbe
Felipe Melo <narulee@hotmail.com>
2022-06-27 19:28:55 -0300

    deploy settings

--- backend/src/middlewares/verifyToken.js
+++ backend/src/middlewares/verifyToken.js
@@ -9,7 +9,7 @@ function verifyToken(req, res, next) {
   if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
 
   jwt.verify(token, process.env.SECRET, function (err, decoded) {
-    if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+    if (err) return res.status(401).json({ auth: false, message: 'Failed to authenticate token.' });
 
     req.userId = decoded.id;
     next();
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,17 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: import.meta.env.VITE_API_ENDPOINT,
+});
+
+api.interceptors.response.use(response => {
+  return response;
+}, error => {
+ if (error.response.status === 401) {
+  localStorage.clear()
+  window.location.reload()
+ }
+ return error;
 });
 
 export default api;
